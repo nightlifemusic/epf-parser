e532f98ff409ae4c429232df5b57bb26909a2668
renewooller <rene>
2017-09-21 16:52:20 +1000

    adds create table

--- extract_schema.js
+++ extract_schema.js
@@ -10,8 +10,10 @@ let tables = fs.readdirSync(category_dir);
 tables.map((table) => {
   fs.createReadStream(category_dir + '/' + table)
   .pipe(parse((meta) => {
-    console.log('meta for '+ table, meta);
-
+    //console.log('meta for '+ table, meta);
+    let cols_sql = meta.map(meta => `${meta.name} ${meta.type}`).join(', ')
+    let sql = `CREATE TABLE  ${table} (${cols_sql})`
+    console.log(sql);
   }));
 })
 
