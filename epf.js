d21ef23adede5921a363766a4302122e47c34de2
renewooller <rene>
2017-09-22 11:21:48 +1000

    problem with write after end

--- epf.js
+++ epf.js
@@ -5,6 +5,7 @@ const bufferSplit = require('buffer-split');
 const multipipe = require('multipipe');
 const bytes = require('bytes');
 const Writable = require('stream').Writable;
+const Transform = require('stream').Transform;
 const PassThrough = require('stream').PassThrough;
 
 const pound = '#'.charCodeAt(0);
@@ -30,10 +31,17 @@ class Epf {
         return multipipe( // TODO : find why piping from binarySplit into the writeable without using multipipe doesn't work
             binarySplit('\x02\n', { // read a line at a time. 
                 maxLength: bytes('1mb')
-            }), new Writable({
-                write: (line, encoding, cb) => {
-                    console.log(line.toString());
-                    cb();
+            }), new Transform({
+                transform(line, encoding, cb) {
+                    if(line[0] != '#'.charCodeAt(0)) {
+                        this.end();
+                        if(this._upstream) this._upstream.unpipe();
+                        cb();
+                    } else {
+                        this.push(line);    
+                        cb();
+                    }
+                        // line = null; // this is a signal to end the stream
                 }
             })
         )
